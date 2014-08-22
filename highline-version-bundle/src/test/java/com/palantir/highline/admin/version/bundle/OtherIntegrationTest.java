ccfe8b5764f570494a10f538984df0954170f066
Javier Campanini <jcampanini@palantir.com>
2014-08-22 18:36:18 +0100

    added the second test harness for detect version.

---
+++ highline-version-bundle/src/test/java/com/palantir/highline/admin/version/bundle/OtherIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.palantir.highline.admin.version.bundle;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.dropwizard.Configuration;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+
+public class OtherIntegrationTest {
+
+    @Rule
+    public DropwizardAppRule<Configuration> RULE = new DropwizardAppRule<>(
+            TestApplications.DetectedVersion.class,
+            "src/test/resources/detect.yml");
+
+    @Test
+    public void fixedServletWorks() throws IOException {
+        HttpClient client = HttpClients.createDefault();
+        HttpGet get = new HttpGet("http://127.0.0.1:" + RULE.getAdminPort() + "/version");
+
+        HttpResponse response = client.execute(get);
+
+        System.out.println(response.getStatusLine().getStatusCode());
+        System.out.println(EntityUtils.toString(response.getEntity()));
+    }
+}
