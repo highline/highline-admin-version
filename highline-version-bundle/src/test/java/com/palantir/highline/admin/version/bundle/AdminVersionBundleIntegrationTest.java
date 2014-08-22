95840d89056e0a541d40d833138f3eb7a37e8637
Javier Campanini <jcampanini@palantir.com>
2014-08-22 18:50:07 +0100

    merged both fixed and detect tests into a single class.

--- highline-version-bundle/src/test/java/com/palantir/highline/admin/version/bundle/AdminVersionBundleIntegrationTest.java
+++ highline-version-bundle/src/test/java/com/palantir/highline/admin/version/bundle/AdminVersionBundleIntegrationTest.java
@@ -10,24 +10,64 @@ import org.apache.http.util.EntityUtils;
 import org.junit.Rule;
 import org.junit.Test;
 
+import com.palantir.highline.admin.version.servlet.VersionServlet;
+
 import io.dropwizard.Configuration;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class AdminVersionBundleIntegrationTest {
 
-    @Rule
-    public DropwizardAppRule<Configuration> RULE = new DropwizardAppRule<>(
-            TestApplications.FixedVersion.class,
-            "src/test/resources/fixed.yml");
+    private static final String CONFIG_FIXED = "src/test/resources/fixed.yml";
+    private static final String CONFIG_DETECT = "src/test/resources/detect.yml";
 
-    @Test
-    public void fixedServletWorks() throws IOException {
-        HttpClient client = HttpClients.createDefault();
-        HttpGet get = new HttpGet("http://127.0.0.1:" + RULE.getAdminPort() + "/version");
+    public static abstract class BaseTest {
+        private final String expectedVersion;
 
-        HttpResponse response = client.execute(get);
+        public BaseTest(String expectedVersion) {
+            this.expectedVersion = expectedVersion;
+        }
 
-        System.out.println(response.getStatusLine().getStatusCode());
-        System.out.println(EntityUtils.toString(response.getEntity()));
+        public abstract DropwizardAppRule getRule();
+
+        @Test
+        public void fixedServletWorks() throws IOException {
+            HttpClient client = HttpClients.createDefault();
+            HttpGet get = new HttpGet("http://127.0.0.1:" + getRule().getAdminPort() + "/version");
+            HttpResponse response = client.execute(get);
+
+            assertThat(response.getStatusLine().getStatusCode(), equalTo(VersionServlet.RESPONSE_STATUS));
+            assertThat(EntityUtils.toString(response.getEntity()), equalTo(expectedVersion));
+        }
+    }
+
+    public static class FixedTest extends BaseTest {
+        @Rule
+        public DropwizardAppRule<Configuration> dwRule = new DropwizardAppRule<>(TestApplications.FixedVersion.class, CONFIG_FIXED);
+
+        public FixedTest() {
+            super("1.0.0");
+        }
+
+        @Override
+        public DropwizardAppRule getRule() {
+            return dwRule;
+        }
+    }
+
+    public static class DetectTest extends BaseTest {
+        @Rule
+        public DropwizardAppRule<Configuration> dwRule = new DropwizardAppRule<>(TestApplications.DetectedVersion.class, CONFIG_DETECT);
+
+        public DetectTest() {
+            super("expected");
+        }
+
+        @Override
+        public DropwizardAppRule getRule() {
+            return dwRule;
+        }
     }
 }
